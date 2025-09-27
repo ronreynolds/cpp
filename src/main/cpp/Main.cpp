1b9875aca42c930fe1ce4c94890afb2a8bea502c
Ron Reynolds <ron@ronreynolds.com>
2025-09-27 08:51:02 -0700

    feature(main): adding some cout statements to verify code is executing

--- src/main/cpp/Main.cpp
+++ src/main/cpp/Main.cpp
@@ -1,4 +1,8 @@
 #include "../include/HelloWorld.h"
+
+using std::cout;
+using std::endl;
+
 /**
  * a single entry point for all our CPP work (to avoid many main()s confusing the linker)
  *
@@ -6,7 +10,9 @@
  * @param argv an array of the command-line args passed in via command-line
  * @return the exit code for the application
  */
-int main(int argc, char** argv) {
-   const int result = local::HelloWorld::main(argc, argv);
-   return result;
+int main(int argc, char **argv) {
+    cout << "starting generic main" << endl;
+    const int result = local::HelloWorld::main(argc, argv);
+    cout << "result = " << result << endl;
+    return result;
 }
